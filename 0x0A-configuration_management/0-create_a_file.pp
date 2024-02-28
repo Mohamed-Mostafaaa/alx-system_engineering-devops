22ca8d04842172e6b0098188052b3e0e1cf643b7
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2024-02-28 21:45:26 +0200

    added permitions

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-reating a file in /tmp
+# creating a file in /tmp
+
 file { '/tmp/school':
-    ensure  => 'file',
-    path    => '/tmp/school',
     content => 'I love Puppet',
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0744'
+    mode    => '0744',
   }
